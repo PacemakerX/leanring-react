ba4556ac82464a7a561ed366c3099f500d0ac841
Sparsh Soni <sparsh.officialwork@gmail.com>
2024-07-04 14:49:59 +0530

    complete lecture 9

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./component/Navbar";
 import TextForm from "./component/TextForm";
+import About from "./component/About";
 
 // let name="sparsh";
 function App() {
@@ -15,6 +16,7 @@ function App() {
         {/* <Navbar/>  */}
         <div className="container my-3">
           <TextForm heading="Enter the text to analyze " />
+          <About />
         </div>
       </div>
     </>
